556355865b4779416417d044b5ff8f9e8afe2678
arnal1 <ahmatarnal@gmail.com>
2020-01-19 09:53:13 +0800

    compress

--- application/views/templates/_footer.php
+++ application/views/templates/_footer.php
@@ -1,3 +1,3 @@
-<footer class="container">
+<footer class="container-fluid">
   <p class="text-muted text-center copyright">Copyright &copy; <?=$site['nama_sekolah'].' - '.$site['nama_website'];?> <?date('Y');?></p>
 </footer>
